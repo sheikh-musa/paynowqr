5ae961fdee3c9ce916aca2886f6f1b34d2f4f79c
sheikh-musa <97861619+sheikh-musa@users.noreply.github.com>
2023-11-29 12:36:56 +0800

    serve index.html

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const http = require('http');
-const PORT = 3000;
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000; // Use the environment variable for PORT
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+  if (req.url === "/" || req.url === "/index.html") {
+    fs.readFile(path.join(__dirname, "public", "index.html"), (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end("Error loading index.html");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404);
+    res.end("Not Found");
+  }
 });
 
 server.listen(PORT, () => {
